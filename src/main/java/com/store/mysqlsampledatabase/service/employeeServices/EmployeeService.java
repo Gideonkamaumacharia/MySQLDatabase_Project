c80db2441ff9ae4a3e22c352c8598c8b1dd922f4
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-06-15 05:04:53 +0300

    Create EmployeeService

--- src/main/java/com/store/mysqlsampledatabase/service/employeeServices/EmployeeService.java
+++ src/main/java/com/store/mysqlsampledatabase/service/employeeServices/EmployeeService.java
@@ -1,4 +1,16 @@
-package com.store.mysqlsampledatabase.service.EmployeeServices;
+package com.store.mysqlsampledatabase.service.employeeServices;
+
+import com.store.mysqlsampledatabase.DTOs.EmployeeDTO;
+import com.store.mysqlsampledatabase.model.Employee;
+
+import java.util.List;
 
 public interface EmployeeService {
+    Employee getEmployee(int employeeNumber);
+    List<Employee> getEmployees();
+    Employee addEmployee(EmployeeDTO employee);
+    //Employee updateEmployee(Employee employee);
+    //Employee deleteEmployee(Employee employee);
+
+
 }
