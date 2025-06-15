7e22ae9c814c7280d0e1d7bea70d32d97644923c
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-06-15 05:00:08 +0300

    Create WebConfig.java

--- src/main/java/com/store/mysqlsampledatabase/config/WebConfig.java
+++ src/main/java/com/store/mysqlsampledatabase/config/WebConfig.java
@@ -1,4 +1,27 @@
 package com.store.mysqlsampledatabase.config;
 
-public class WebConfig {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Slf4j
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    public WebConfig() {
+        log.info("✅ WebConfig class has been loaded.");
+    }
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        log.info("🌍 Configuring global CORS for frontend access...");
+
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173/")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
