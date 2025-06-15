9dcd72f0a2f1de908d0f726defe5755b98863fb2
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-06-15 05:01:18 +0300

    Create EmployeeController

--- src/main/java/com/store/mysqlsampledatabase/controller/EmployeeController.java
+++ src/main/java/com/store/mysqlsampledatabase/controller/EmployeeController.java
@@ -1,4 +1,35 @@
 package com.store.mysqlsampledatabase.controller;
 
+import com.store.mysqlsampledatabase.DTOs.EmployeeDTO;
+import com.store.mysqlsampledatabase.model.Employee;
+import com.store.mysqlsampledatabase.service.employeeServices.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/employees")
+@RequiredArgsConstructor
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @GetMapping("/getEmployees")
+    public List<Employee> getEmployees() {
+        return employeeService.getEmployees();
+    }
+
+    @GetMapping("/getEmployee/{employeeNumber}")
+    public Employee getEmployee(@PathVariable("employeeNumber") int employeeNumber) {
+        return employeeService.getEmployee(employeeNumber);
+    }
+
+    @PostMapping("/saveEmployee")
+    public Employee saveEmployee(@RequestBody EmployeeDTO dto) {
+        return employeeService.addEmployee(dto);
+    }
+
+
+
 }
