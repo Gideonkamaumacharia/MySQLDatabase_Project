455927e5756c6be0af68864f06b706af0a755884
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-06-26 16:43:29 +0300

    Update ProductLineController for product line operations

--- src/main/java/com/store/mysqlsampledatabase/controller/ProductLineController.java
+++ src/main/java/com/store/mysqlsampledatabase/controller/ProductLineController.java
@@ -1,4 +1,24 @@
 package com.store.mysqlsampledatabase.controller;
 
+import com.store.mysqlsampledatabase.model.Productline;
+import com.store.mysqlsampledatabase.service.productLineServices.ProductLineService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class ProductLineController {
+    private final ProductLineService productLineService;
+
+    @GetMapping("/productLines")
+    ResponseEntity<List<Productline>> getProductLines() {
+        List<Productline> productLines = productLineService.getProductLines();
+        return ResponseEntity.ok(productLines);
+    }
 }
