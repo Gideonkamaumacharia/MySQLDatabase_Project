05f3386b4b278300f522ed72fd1b7c05f4b7a255
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-06-26 16:44:25 +0300

    Update ProductLineService interface definition

--- src/main/java/com/store/mysqlsampledatabase/service/productLineServices/ProductLineService.java
+++ src/main/java/com/store/mysqlsampledatabase/service/productLineServices/ProductLineService.java
@@ -1,4 +1,11 @@
 package com.store.mysqlsampledatabase.service.productLineServices;
 
+import com.store.mysqlsampledatabase.model.Productline;
+
+import java.util.List;
+
 public interface ProductLineService {
+    List<Productline> getProductLines();
+
+
 }
