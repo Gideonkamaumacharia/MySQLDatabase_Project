61e7f17d38c6e0657016b5de3cfa6cd3a95e3b27
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-06-26 16:45:44 +0300

    Update WebConfig with changes to configuration settings

--- src/main/java/com/store/mysqlsampledatabase/config/WebConfig.java
+++ src/main/java/com/store/mysqlsampledatabase/config/WebConfig.java
@@ -1,12 +1,22 @@
 package com.store.mysqlsampledatabase.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @Slf4j
 @Configuration
+//@EnableWebSecurity
+//@EnableMethodSecurity
+
 public class WebConfig implements WebMvcConfigurer {
 
     public WebConfig() {
@@ -24,4 +34,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(customizer -> customizer.disable())
+//                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+//                .httpBasic(Customizer.withDefaults())
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//        return http.build();
+//    }
 }
