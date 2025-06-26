c633bdc9c1e02773b0376520dc90293442bc9d08
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-06-26 16:42:56 +0300

    Update OrderDTO fields and structure

--- src/main/java/com/store/mysqlsampledatabase/DTOs/OrderDTO.java
+++ src/main/java/com/store/mysqlsampledatabase/DTOs/OrderDTO.java
@@ -1,4 +1,11 @@
 package com.store.mysqlsampledatabase.DTOs;
 
+import com.store.mysqlsampledatabase.model.Customer;
+import com.store.mysqlsampledatabase.model.Orderdetail;
+
 public class OrderDTO {
+    private String status;
+    private String comments;
+    private String customerName;
+    //private List<OrderDetailDTO> orderdetails;
 }
