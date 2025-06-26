a9fd8c98b7e2d56b22c655f433cdd6727624f180
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-06-26 16:43:26 +0300

    Improve ProductController API logic

--- src/main/java/com/store/mysqlsampledatabase/controller/ProductController.java
+++ src/main/java/com/store/mysqlsampledatabase/controller/ProductController.java
@@ -1,23 +1,34 @@
 package com.store.mysqlsampledatabase.controller;
 
 import com.store.mysqlsampledatabase.model.Product;
+import com.store.mysqlsampledatabase.repository.ProductRepository;
 import com.store.mysqlsampledatabase.service.productServices.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+//import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
+//@PreAuthorize("hasRole('ADMIN')")
 public class ProductController {
     private final ProductService productService;
+    private final ProductRepository productRepository;
 
     @GetMapping("/getProducts")
     ResponseEntity<List<Product>> getProducts() {
         List<Product> products = productService.getProducts();
         return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/getProducts/{productCode}")
+    ResponseEntity<Product> getProduct(@PathVariable String productCode) {
+        Product product = productService.getProductById(productCode);
+        return ResponseEntity.ok(product);
     }
 
     @PostMapping("/saveProduct")
@@ -27,9 +38,23 @@ public class ProductController {
     }
 
     @PatchMapping("/updateProducts/{productCode}")
-    ResponseEntity<Product> updateProduct(@PathVariable String productCode,@RequestBody Product product) {
-        Product  updatedProduct = productService.updateProduct(productCode, product);
+    ResponseEntity<Product> updateProduct(@PathVariable String productCode,@RequestBody Map<String, Object> updates) {
+        Product  updatedProduct = productService.updateProductFields(productCode, updates);
         return ResponseEntity.ok(updatedProduct);
-
     }
+
+    @DeleteMapping("/deleteProduct/{productCode}")
+    public ResponseEntity<?> deleteProduct(@PathVariable String productCode) {
+        productService.deleteProductById(productCode);
+        return ResponseEntity.ok().body(
+                Map.of("Message", "Product deleted successfully!")
+        );
+    }
+
+    @GetMapping("/total")
+    ResponseEntity<?> getTotalProducts() {
+         return ResponseEntity.ok(productRepository.countTotalProducts());
+    }
+
+
 }
