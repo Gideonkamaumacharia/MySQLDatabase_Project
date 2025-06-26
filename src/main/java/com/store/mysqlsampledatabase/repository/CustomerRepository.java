32f9c862097f547e9ef24f995cd3b13f31b04b7a
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-06-26 16:44:02 +0300

    Update CustomerRepository with new query methods

--- src/main/java/com/store/mysqlsampledatabase/repository/CustomerRepository.java
+++ src/main/java/com/store/mysqlsampledatabase/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.store.mysqlsampledatabase.repository;
 
 import com.store.mysqlsampledatabase.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 }
