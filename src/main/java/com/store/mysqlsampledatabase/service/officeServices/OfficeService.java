9fba3b7aa02c189655dd5a0a95c37664af13517e
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-06-26 16:44:24 +0300

    Adjust OfficeService interface

--- src/main/java/com/store/mysqlsampledatabase/service/officeServices/OfficeService.java
+++ src/main/java/com/store/mysqlsampledatabase/service/officeServices/OfficeService.java
@@ -1,4 +1,15 @@
 package com.store.mysqlsampledatabase.service.officeServices;
 
+import com.store.mysqlsampledatabase.model.Office;
+
+import java.util.List;
+import java.util.Map;
+
 public interface OfficeService {
+    List<Office> getAllOffice();
+    Office getOffice(String officeCode);
+    Office addOffice(Office office);
+    Office updateOfficeFields(String officeCode, Map<String, Object> updates);
+
+    void deleteOffice(String officeCode);
 }
