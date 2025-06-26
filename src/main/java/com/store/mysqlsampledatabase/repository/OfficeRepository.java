2b63e3a6aa899ea926630b65fefe834c6c1c7f45
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-06-26 16:44:04 +0300

    Tweak OfficeRepository functionality

--- src/main/java/com/store/mysqlsampledatabase/repository/OfficeRepository.java
+++ src/main/java/com/store/mysqlsampledatabase/repository/OfficeRepository.java
@@ -1,4 +1,9 @@
 package com.store.mysqlsampledatabase.repository;
 
-public interface OfficeRepository {
+import com.store.mysqlsampledatabase.model.Office;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OfficeRepository extends JpaRepository<Office, String> {
 }
