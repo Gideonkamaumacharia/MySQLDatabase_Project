623c128cb2cf59fb33fde122b8af801547e3fea2
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-06-26 16:44:05 +0300

    Update ProductRepository for better product lookup

--- src/main/java/com/store/mysqlsampledatabase/repository/ProductRepository.java
+++ src/main/java/com/store/mysqlsampledatabase/repository/ProductRepository.java
@@ -2,8 +2,13 @@ package com.store.mysqlsampledatabase.repository;
 
 import com.store.mysqlsampledatabase.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import static javax.swing.text.html.HTML.Tag.P;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
+    @Query("SELECT COUNT(p) FROM Product p" )
+    Long countTotalProducts();
 }
