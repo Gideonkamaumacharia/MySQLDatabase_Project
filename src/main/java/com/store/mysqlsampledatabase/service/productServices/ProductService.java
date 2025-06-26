49a7f42e86ec0edc81062633e156b8c86c70918a
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-06-26 16:44:26 +0300

    Update ProductService interface methods

--- src/main/java/com/store/mysqlsampledatabase/service/productServices/ProductService.java
+++ src/main/java/com/store/mysqlsampledatabase/service/productServices/ProductService.java
@@ -3,11 +3,13 @@ package com.store.mysqlsampledatabase.service.productServices;
 import com.store.mysqlsampledatabase.model.Product;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ProductService {
     List<Product> getProducts();
     Product getProductById(String productCode);
     void deleteProductById(String productCode);
-    Product updateProduct(String productCode,Product product);
     Product saveProduct(Product product);
+    Product updateProductFields(String productCode, Map<String, Object> updates);
+
 }
