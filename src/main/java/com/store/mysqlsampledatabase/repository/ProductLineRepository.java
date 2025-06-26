70a34d04429371d80ef2e0eefb43787a84381285
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-06-26 16:44:05 +0300

    Modify ProductLineRepository for updated access

--- src/main/java/com/store/mysqlsampledatabase/repository/ProductLineRepository.java
+++ src/main/java/com/store/mysqlsampledatabase/repository/ProductLineRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductLineRepository extends JpaRepository<Productline, Integer> {
+public interface ProductLineRepository extends JpaRepository<Productline, String> {
 }
