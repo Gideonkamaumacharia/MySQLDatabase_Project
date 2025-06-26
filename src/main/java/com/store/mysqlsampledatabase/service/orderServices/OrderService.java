80285eff9f4971455e0db009fe40800ba10bce06
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-06-26 16:44:24 +0300

    Refine OrderService interface structure

--- src/main/java/com/store/mysqlsampledatabase/service/orderServices/OrderService.java
+++ src/main/java/com/store/mysqlsampledatabase/service/orderServices/OrderService.java
@@ -1,4 +1,14 @@
-package com.store.mysqlsampledatabase.service;
+package com.store.mysqlsampledatabase.service.orderServices;
+
+import com.store.mysqlsampledatabase.model.Order;
+
+import java.util.List;
+import java.util.Map;
 
 public interface OrderService {
+    List<Order> getOrders();
+    Order getOrder(Integer orderNumber);
+    Order addOrder(Order order);
+    void deleteOrder(Integer orderNumber);
+    Order updateOrder(Integer orderNumber, Map<String, Object> updates);
 }
