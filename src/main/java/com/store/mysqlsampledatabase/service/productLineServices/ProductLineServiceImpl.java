c0d9797bfdb5e2f8c67df9f415f27fa50a6143d7
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-06-26 16:44:26 +0300

    Refactor logic in ProductLineServiceImpl

--- src/main/java/com/store/mysqlsampledatabase/service/productLineServices/ProductLineServiceImpl.java
+++ src/main/java/com/store/mysqlsampledatabase/service/productLineServices/ProductLineServiceImpl.java
@@ -1,4 +1,18 @@
 package com.store.mysqlsampledatabase.service.productLineServices;
 
-public class ProductLineServiceImpl {
+import com.store.mysqlsampledatabase.model.Productline;
+import com.store.mysqlsampledatabase.repository.ProductLineRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+@RequiredArgsConstructor
+public class ProductLineServiceImpl implements ProductLineService{
+    private final ProductLineRepository productLineRepository;
+
+    @Override
+    public List<Productline> getProductLines() {
+        return productLineRepository.findAll();
+    }
 }
