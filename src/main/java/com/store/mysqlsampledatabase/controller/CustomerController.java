2a55fe0aafcb6c22f13db0a88ccf80ce0e0c6971
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-06-26 16:43:18 +0300

    Enhance CustomerController logic and endpoints

--- src/main/java/com/store/mysqlsampledatabase/controller/CustomerController.java
+++ src/main/java/com/store/mysqlsampledatabase/controller/CustomerController.java
@@ -1,18 +1,27 @@
 package com.store.mysqlsampledatabase.controller;
 
+import com.store.mysqlsampledatabase.DTOs.CustomerDTO;
 import com.store.mysqlsampledatabase.model.Customer;
+import com.store.mysqlsampledatabase.model.Employee;
+import com.store.mysqlsampledatabase.repository.CustomerRepository;
+import com.store.mysqlsampledatabase.repository.EmployeeRepository;
 import com.store.mysqlsampledatabase.service.customerServices.CustomerService;
+import com.store.mysqlsampledatabase.service.employeeServices.EmployeeService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/customers")
 public class CustomerController {
     private final CustomerService customerService;
+    private final EmployeeRepository employeeRepository;
+
 
     @GetMapping("/getCustomers")
     public ResponseEntity<List<Customer>> getCustomers() {
@@ -21,10 +30,25 @@ public class CustomerController {
     }
 
     @PostMapping("/saveCustomers")
-    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
-        Customer savedCustomer = customerService.saveCustomer(customer);
-        return ResponseEntity.ok(savedCustomer);
+    public ResponseEntity<Customer> createCustomer(@RequestBody CustomerDTO dto) {
+        Customer customer = new Customer();
+        customer.setCustomerName(dto.getCustomerName());
+        customer.setContactFirstName(dto.getContactFirstName());
+        customer.setContactLastName(dto.getContactLastName());
+        customer.setAddressLine1(dto.getAddressLine1());
+        customer.setPhone(dto.getPhone());
+        customer.setCreditLimit(dto.getCreditLimit());
+        customer.setCity(dto.getCity());
+        customer.setCountry(dto.getCountry());
+
+        if (dto.getSalesRepId() != null) {
+            Employee rep = employeeRepository.findById(dto.getSalesRepId())
+                    .orElseThrow(() -> new NoSuchElementException("Rep not found"));
+            customer.setSalesRepresentative(rep);
     }
 
 
-}
+    return ResponseEntity.ok(customerService.saveCustomer(customer));
+
+
+}}
