f210b8fbebdc71086f5c24276763b6d4169fadba
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-06-26 16:42:55 +0300

    Refactor CustomerDTO for better data handling

--- src/main/java/com/store/mysqlsampledatabase/DTOs/CustomerDTO.java
+++ src/main/java/com/store/mysqlsampledatabase/DTOs/CustomerDTO.java
@@ -1,4 +1,20 @@
 package com.store.mysqlsampledatabase.DTOs;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
 public class CustomerDTO {
+    private String customerName;
+    private String contactFirstName;
+    private String contactLastName;
+    private String phone;
+    private String addressLine1;
+    private String city;
+    private String country;
+    private Integer salesRepId;
+    private BigDecimal creditLimit;
 }
